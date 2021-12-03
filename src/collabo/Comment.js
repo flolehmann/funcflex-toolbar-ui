fdaf262518059eb16fdbd25ae696adfe48011319
Florian Lehmann <florian.lehmann@uni-bayreuth.de>
2021-12-03 14:59:12 +0100

    fixed month december in comment

--- src/collabo/Comment.js
+++ src/collabo/Comment.js
@@ -19,7 +19,7 @@ const dateHelper = (date) => {
     const [month, day, year] = [formatNumber(date.getMonth() + 1), formatNumber(date.getDate()), date.getFullYear()];
     const [hour, minutes, seconds] = [formatNumber(date.getHours()), formatNumber(date.getMinutes()), formatNumber(date.getSeconds())];
 
-    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dez"];
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
     return hour + ":" + minutes + " " + day + " " + months[month-1];
 };
