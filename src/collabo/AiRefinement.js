d1342c35d9f0efc02dea2286c8cf687f892642ce
Florian Lehmann <florian.lehmann@uni-bayreuth.de>
2023-03-07 10:20:05 +0100

    prevent card selection after modal close

--- src/collabo/AiRefinement.js
+++ src/collabo/AiRefinement.js
@@ -42,70 +42,76 @@ export default function AiRefinement(props) {
     };
 
     return (
-        <Modal
-            onShow={() => scrollToAnnotation()}
-            contentClassName={"refinement-modal-content"}
-            dialogClassName={"refinement-modal"}
-            {...rest}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-            onClick={e => {
-                e.preventDefault();
-                e.stopPropagation();
-                e.nativeEvent.stopImmediatePropagation();
-            }}
-        >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    {title}
-                </Modal.Title>
-            </Modal.Header>
+        <div onClick={e => {
+            e.preventDefault();
+            e.stopPropagation();
+            e.nativeEvent.stopImmediatePropagation();
+        }}>
+            <Modal
+                onShow={() => scrollToAnnotation()}
+                contentClassName={"refinement-modal-content"}
+                dialogClassName={"refinement-modal"}
+                {...rest}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+                onClick={e => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    e.nativeEvent.stopImmediatePropagation();
+                }}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        {title}
+                    </Modal.Title>
+                </Modal.Header>
 
-            <Modal.Body>
-                <Container className="vh-50 d-flex flex-column ">
-                    <Row>
-                        <Col xs={12} md={6}>
-                            <Row>
-                                <Col>
-                                    <Badge bg="secondary">
-                                        Original
-                                    </Badge>
-                                </Col>
-                            </Row>
-                            <div className="original-text" ref={markerRef} dangerouslySetInnerHTML={{ __html: originalText }}></div>
-                        </Col>
-                        <Col xs={12} md={6}>
-                            <Row>
-                                <Col>
-                                    <Badge bg="warning" text="dark">
-                                        AI Suggestion
-                                    </Badge>
-                                </Col>
-                            </Row>
-                            <Row className="h-100">
-                                <Col className={"refinement-text"}>
-                                    <div onClick={scrollToAnnotation} className="refinement">{ refinement }</div>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                </Container>
-            </Modal.Body>
-            <Modal.Footer>
-                <Container>
-                    <Row>
-                        <Col xs={12} md={7}>
-                            { hasTakeOverWarning &&
-                            <p className={"warning"}>By taking over the AI suggestion, you are resolving the comment and closing the discussion.
-                                The highlighted text in document will be overwritten.</p> }
-                        </Col>
-                        <Col xs={8} md={5} className={"button-bar"}>
-                            {props.children}
-                        </Col>
-                    </Row>
-                </Container>
-            </Modal.Footer>
-        </Modal>
+                <Modal.Body>
+                    <Container className="vh-50 d-flex flex-column ">
+                        <Row>
+                            <Col xs={12} md={6}>
+                                <Row>
+                                    <Col>
+                                        <Badge bg="secondary">
+                                            Original
+                                        </Badge>
+                                    </Col>
+                                </Row>
+                                <div className="original-text" ref={markerRef} dangerouslySetInnerHTML={{ __html: originalText }}></div>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <Row>
+                                    <Col>
+                                        <Badge bg="warning" text="dark">
+                                            AI Suggestion
+                                        </Badge>
+                                    </Col>
+                                </Row>
+                                <Row className="h-100">
+                                    <Col className={"refinement-text"}>
+                                        <div onClick={scrollToAnnotation} className="refinement">{ refinement }</div>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Container>
+                        <Row>
+                            <Col xs={12} md={7}>
+                                { hasTakeOverWarning &&
+                                <p className={"warning"}>By taking over the AI suggestion, you are resolving the comment and closing the discussion.
+                                    The highlighted text in document will be overwritten.</p> }
+                            </Col>
+                            <Col xs={8} md={5} className={"button-bar"}>
+                                {props.children}
+                            </Col>
+                        </Row>
+                    </Container>
+                </Modal.Footer>
+            </Modal>
+        </div>
     );
 }
